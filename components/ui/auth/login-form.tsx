2366a709c90f2f3c9e120d02e482ab634264f234
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-08-04 01:00:29 +0600

    feat: project initialized and login card made

---
+++ components/ui/auth/login-form.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { CardWrapper } from "./card-wrapper"
+
+export const LoginForm = () => {
+  return (
+    <CardWrapper
+      headerLabel="Welcome back"
+      backButtonLabel="Don't have an account?"
+      backButtonHref="/auth/register"
+      showSocial
+    >
+      Login Form
+    </CardWrapper>
+  )
+}
