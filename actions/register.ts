bdbdea69121280b866a325389f81a5474684fa9e
sarikamahboob <sarika.mahboob1234@gmail.com>
2024-08-05 21:40:58 +0600

    feat: user register functions added

--- actions/register.ts
+++ actions/register.ts
@@ -2,16 +2,43 @@
 
 import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcryptjs";
+import {db} from "@/lib/db"
+import { getUserByEmail } from "@/data/user";
 
-interface LoginResponse {
+interface RegisterResponse {
   success?: string;
   error?: string;
 }
 
-export const register = async (values:z.infer<typeof RegisterSchema>): Promise<LoginResponse> => {
+export const register = async (values:z.infer<typeof RegisterSchema>): Promise<RegisterResponse> => {
+
+  // validate fields using zod
   const validatedFields = RegisterSchema.safeParse(values)
   if(!validatedFields.success) {
     return {error: "Invalid fields!"}
   }
-  return {success: "Email sent!"}
+
+  const {email, password, name} = validatedFields.data;
+
+  // hashed the password
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  // checking the given email is unique
+  const existingUser = await getUserByEmail(email)
+  if( existingUser ){
+    return {error: "Email already taken!"}
+  }
+
+  // creating the data 
+  await db.user.create({
+    data : {
+      name, 
+      email,
+      password: hashedPassword,
+    }
+  })
+
+  // send verification token
+  return {success: "User created!"}
 }
